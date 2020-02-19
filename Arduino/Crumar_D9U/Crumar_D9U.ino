70c9aabe084c6eef55d0637cb2a077b05731f859
corvusnix <56633824+corvusnix@users.noreply.github.com>
2020-02-19 03:48:39 +0100

    Code clean up

--- Arduino/Crumar_D9U/Crumar_D9U.ino
+++ Arduino/Crumar_D9U/Crumar_D9U.ino
@@ -7,9 +7,11 @@
 // Last update: February 2020
 //
 
+#define DRAWBAR_COUNT 9
+
 ////////////////////////////////////////////////////////////////////
 // This is where you can define your CC numbers for the Bank 0 or 1
-uint8_t CCMap[2][9] =
+uint8_t CCMap[2][DRAWBAR_COUNT] =
 {
   { 12, 13, 14, 15, 16, 17, 18, 19, 20 }, // Upper drawbars
   { 21, 22, 23, 24, 25, 26, 27, 28, 29 }  // Lower drawbars
@@ -36,21 +38,17 @@ uint8_t CCMap[2][9] =
 
 MIDI_CREATE_DEFAULT_INSTANCE();
 
-// Init global variables
+// Make sure mode is either 0 or 1
 bool mode;
-int prev_val[9] = { -1, -1, -1, -1, -1, -1, -1, -1, -1 };
-uint8_t debounce_timer = DEBOUNCE_TIME;
 
 // ADC reference map
-int ADCmap[9] = { A0, A1, A2, A3, A6, A7, A8, A9, A10 };
-uint8_t ADCcnt = 0;
+int ADCmap[DRAWBAR_COUNT] = { A0, A1, A2, A3, A6, A7, A8, A9, A10 };
 
 // Called then the pushbutton is depressed
-void set_mode()
+void set_leds()
 {
   digitalWrite(LED_RED, !mode);
   digitalWrite(LED_GREEN, mode);
-  EEPROM.update(0x01, mode);
 }
 
 // Called to generate the MIDI CC message
@@ -65,22 +63,39 @@ void SendMidiCC(uint8_t num, uint8_t value)
 }
 
 // Called to check whether a drawbar has been moved
-void DoDrawbar(uint8_t d, int value)
-{
-  // Get difference from current and previous value
-  int diff = abs(value - prev_val[d]);
+void checkDrawbar() {  
+  // Read analog inputs (do the round robin)
+  static uint8_t drawbar = 0;
+  int value = analogRead(ADCmap[drawbar]);
+  if (++drawbar >= DRAWBAR_COUNT) drawbar = 0;
 
-  // Exit this function if the new value is not within the deadband
-  if (diff <= DEADBAND) return;
+  // Get difference from current and previous value and
+  // Exit this function if the new value is within the deadband
+  static int prev_val[DRAWBAR_COUNT] = { -1, -1, -1, -1, -1, -1, -1, -1, -1 };
+  if (abs(value - prev_val[drawbar]) <= DEADBAND) return;
 
   // Store new value
-  prev_val[d] = value;
+  prev_val[drawbar] = value;
 
-  // Get the 7 bit value
-  uint8_t val7bit = value >> 3;
+  // Send Midi with the 7 bit value
+  SendMidiCC(CCMap[mode][drawbar], value >> 3);
+}
 
-  // Send Midi
-  SendMidiCC(CCMap[mode][d], val7bit);
+void checkButton() {
+  static uint8_t debounce_timer = DEBOUNCE_TIME;
+  // Read Button
+  if (digitalRead(BUTTON) == LOW) {
+    if (debounce_timer > 0) {
+      --debounce_timer;
+      if (debounce_timer == 1) {
+        mode = !mode; // Reverse
+        set_leds(); // and set LEDs
+        EEPROM.update(0x01, mode); //save mode for reboot
+      }
+    }
+  } else {
+    debounce_timer = DEBOUNCE_TIME;
+  }
 }
 
 // The setup routine runs once when you press reset:
@@ -94,29 +109,16 @@ void setup()
   pinMode(LED_RED, OUTPUT);      // Led 1
   pinMode(LED_GREEN, OUTPUT);    // Led 2
 
-  // Recall mode from memory and set
-  // Make sure mode is either 0 or 1
+  // Recall mode from memory and set LEDs
   mode = EEPROM.read(0x01);
-  set_mode();
+  set_leds();
 }
 
 // The loop routine runs over and over again forever:
 void loop()
 {
-  // Read analog inputs (do the round robin)
-  DoDrawbar(ADCcnt, analogRead(ADCmap[ADCcnt]));
-  if (++ADCcnt >= 9) ADCcnt = 0;
-
-  // Read Button
-  if (digitalRead(BUTTON) == LOW) {
-    if (debounce_timer > 0) {
-      --debounce_timer;
-      if (debounce_timer == 2) {
-        mode = !mode; // Reverse
-        set_mode(); // and Set!
-      }
-    }
-  } else {
-    debounce_timer = DEBOUNCE_TIME;
-  }
+  // Check next Drawbar for changed position
+  checkDrawbar();
+  // Check if Button is pressed
+  checkButton();
 }
